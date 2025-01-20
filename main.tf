cd1c97f3203ed708c51578f3f12c715cb967f28c
Miguel Sanchez <miguelsancor_2005@hotmail.com>
2025-01-20 00:49:11 -0300

    actualizacion ejercicio con base de datos

--- main.tf
+++ main.tf
@@ -199,10 +199,18 @@ resource "aws_security_group" "tools_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 1433
+    to_port     = 1433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
 }
